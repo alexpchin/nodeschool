0d91ba3957f3e88daf853965d05f3d8357c3288f
alexpchin <alex@alexchin.co.uk>
2015-07-12 13:49:40 +0100

    Tests passing

---
+++ javascripting/09-if-statement.js
@@ -0,0 +1,6 @@
+var orange = String(orange);
+if (orange.length > 5) {
+  console.log("The fruit name has more than five characters.")
+} else {
+  console.log("The fruit name has five characters or less.")
+}
